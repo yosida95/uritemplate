0babc96cefd83ba6fcf0e33fd00a3cd523c116ba
Kohei YOSHIDA <kohei@yosida95.com>
2016-11-30 21:04:57 +0900

    more strict parsing

--- parse.go
+++ parse.go
@@ -6,6 +6,11 @@
 
 package uritemplate
 
+import (
+	"unicode"
+	"unicode/utf8"
+)
+
 const (
 	parseOpSimple = iota
 	parseOpPlus
@@ -15,6 +20,61 @@ const (
 	parseOpSemicolon
 	parseOpQuestion
 	parseOpAmpersand
+)
+
+const (
+	parseStateDefault = iota
+	parseStateHexDigit1
+	parseStateHexDigit2
+)
+
+var (
+	rangeVarchar = &unicode.RangeTable{
+		R16: []unicode.Range16{
+			unicode.Range16{Lo: 0x0030, Hi: 0x0039, Stride: 1}, // '0' - '9'
+			unicode.Range16{Lo: 0x0041, Hi: 0x005A, Stride: 1}, // 'A' - 'Z'
+			unicode.Range16{Lo: 0x005F, Hi: 0x005F, Stride: 1}, // '_'
+			unicode.Range16{Lo: 0x0061, Hi: 0x007A, Stride: 1}, // 'a' - 'z'
+		},
+		LatinOffset: 4,
+	}
+	rangeLiterals = &unicode.RangeTable{
+		R16: []unicode.Range16{
+			unicode.Range16{Lo: 0x0021, Hi: 0x0021, Stride: 1}, // '!'
+			unicode.Range16{Lo: 0x0023, Hi: 0x0024, Stride: 1}, // '#' - '$'
+			unicode.Range16{Lo: 0x0026, Hi: 0x0026, Stride: 1}, // '&'
+			unicode.Range16{Lo: 0x0028, Hi: 0x003B, Stride: 1}, // '(' - ';'
+			unicode.Range16{Lo: 0x003D, Hi: 0x003D, Stride: 1}, // '='
+			unicode.Range16{Lo: 0x003F, Hi: 0x005B, Stride: 1}, // '?' - '['
+			unicode.Range16{Lo: 0x005D, Hi: 0x005D, Stride: 1}, // ']'
+			unicode.Range16{Lo: 0x005F, Hi: 0x005F, Stride: 1}, // '_'
+			unicode.Range16{Lo: 0x0061, Hi: 0x007A, Stride: 1}, // 'a' - 'z'
+			unicode.Range16{Lo: 0x007E, Hi: 0x007E, Stride: 1}, // '~'
+			unicode.Range16{Lo: 0x00A0, Hi: 0xD7FF, Stride: 1}, // ucschar
+			unicode.Range16{Lo: 0xE000, Hi: 0xF8FF, Stride: 1}, // iprivate
+			unicode.Range16{Lo: 0xF900, Hi: 0xFDCF, Stride: 1}, // ucschar
+			unicode.Range16{Lo: 0xFDF0, Hi: 0xFFEF, Stride: 1}, // ucschar
+		},
+		R32: []unicode.Range32{
+			unicode.Range32{Lo: 0x00010000, Hi: 0x0001FFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x00020000, Hi: 0x0002FFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x00030000, Hi: 0x0003FFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x00040000, Hi: 0x0004FFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x00050000, Hi: 0x0005FFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x00060000, Hi: 0x0006FFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x00070000, Hi: 0x0007FFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x00080000, Hi: 0x0008FFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x00090000, Hi: 0x0009FFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x000A0000, Hi: 0x000AFFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x000B0000, Hi: 0x000BFFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x000C0000, Hi: 0x000CFFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x000D0000, Hi: 0x000DFFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x000E1000, Hi: 0x000EFFFD, Stride: 1}, // ucschar
+			unicode.Range32{Lo: 0x000F0000, Hi: 0x000FFFFD, Stride: 1}, // iprivate
+			unicode.Range32{Lo: 0x00100000, Hi: 0x0010FFFD, Stride: 1}, // iprivate
+		},
+		LatinOffset: 10,
+	}
 )
 
 type parser struct {
@@ -85,45 +145,64 @@ func (p *parser) consumeMaxLength() (int, error) {
 
 func (p *parser) consumeVarspec() (varspec, error) {
 	debug.Printf("consumeVarspec: %q", p.r)
+	var state int
 	var ret varspec
+	var err error
 	for i := 0; i < len(p.r); {
-		switch p.r[i] {
-		default:
-			i++
-			continue
-		case ':':
-			ret := varspec{
-				name: p.r[:i],
+		r, size := utf8.DecodeRuneInString(p.r[i:])
+		if r == utf8.RuneError {
+			return ret, errorf(p.read+i, "invalid encoding")
+		}
+		switch state {
+		case parseStateDefault:
+			switch r {
+			default:
+				if !unicode.Is(rangeVarchar, r) {
+					debug.Printf("consumeVarspec: found=%q at=%d", string(r), i)
+					return ret, errorf(p.read+i, "invalid varname")
+				}
+				i += size
+				continue
+			case '%':
+				state = parseStateHexDigit1
+			case ':':
+				ret.name = p.r[:i]
+				debug.Printf("consumeVarspec: name=%q", ret.name)
+				p.dropN(i + 1) // name + ':'
+				ret.maxlen, err = p.consumeMaxLength()
+				if err != nil {
+					return ret, err
+				}
+				debug.Printf("consumeVarspec: maxlen=%d", ret.maxlen)
+				return ret, nil
+			case '*':
+				ret.name = p.r[:i]
+				debug.Printf("consumeVarspec: name=%q", ret.name)
+				ret.explode = true
+				debug.Printf("consumeVarspec: explode=true")
+				p.dropN(i + 1) // name + '*'
+				return ret, nil
+			case ',', '}':
+				ret.name = p.r[:i]
+				debug.Printf("consumeVarspec: name=%q", ret.name)
+				p.dropN(i)
+				return ret, nil
 			}
-			p.dropN(i + 1)
-			debug.Printf("consumeVarspec: name=%q", ret.name)
-
-			maxlen, err := p.consumeMaxLength()
-			if err != nil {
-				return ret, err
+		case parseStateHexDigit1:
+			if !unicode.Is(unicode.ASCII_Hex_Digit, r) {
+				debug.Printf("consumeVarspec: found=%q at=%d", string(r), i)
+				return ret, errorf(p.read+i, "invalid pct-encoded")
 			}
-			ret.maxlen = maxlen
-			debug.Printf("consumeVarspec: maxlen=%d", ret.maxlen)
-			return ret, nil
-		case '*':
-			ret := varspec{
-				name:    p.r[:i],
-				explode: true,
+			state = parseStateHexDigit2
+		case parseStateHexDigit2:
+			if !unicode.Is(unicode.ASCII_Hex_Digit, r) {
+				debug.Printf("consumeVarspec: found=%q at=%d", string(r), i)
+				return ret, errorf(p.read+i, "invalid pct-encoded")
 			}
-			p.dropN(i + 1)
-			debug.Printf("consumeVarspec: name=%q", ret.name)
-			debug.Printf("consumeVarspec: explode=true")
-			return ret, nil
-		case ',', '}':
-			ret := varspec{
-				name: p.r[:i],
-			}
-			p.dropN(i)
-			debug.Printf("consumeVarspec: name=%q", ret.name)
-			return ret, nil
+			state = parseStateDefault
 		}
 	}
-	debug.Printf("consumeVarspec: remains=%q", p.r)
+	debug.Printf("consumeVarspec: unexpected end of URI Template")
 	return ret, errorf(p.read+1, "incomplete template")
 }
 
@@ -198,14 +277,44 @@ func (p *parser) consumeExpression() (expression, error) {
 
 func (p *parser) consumeLiterals() (expression, error) {
 	debug.Printf("consumeLiterals: %q", p.r)
+	state := parseStateDefault
 	i := 0
-	for {
-		c := p.r[i]
-		if c == '{' {
-			break
+Loop:
+	for i < len(p.r) {
+		r, size := utf8.DecodeRuneInString(p.r[i:])
+		if r == utf8.RuneError {
+			return nil, errorf(p.read+i, "invalid encoding")
 		}
-		// TODO(yosida95): is c in literals?
-		i++
+		switch state {
+		case parseStateDefault:
+			switch r {
+			case '{':
+				break Loop
+			case '%':
+				state = parseStateHexDigit1
+			default:
+				if !unicode.Is(rangeLiterals, r) {
+					debug.Printf("consumeLiterals: found=%q at=%d", string(r), i)
+					return nil, errorf(p.read+i, "invalid literals")
+				}
+			}
+		case parseStateHexDigit1:
+			if !unicode.Is(unicode.ASCII_Hex_Digit, r) {
+				debug.Printf("consumeLiterals: found=%q at=%d", string(r), i)
+				return nil, errorf(p.read+i, "invalid pct-encoded")
+			}
+			state = parseStateHexDigit2
+		case parseStateHexDigit2:
+			if !unicode.Is(unicode.ASCII_Hex_Digit, r) {
+				debug.Printf("consumeLiterals: found=%q at=%d", string(r), i)
+				return nil, errorf(p.read+i, "invalid pct-encoded")
+			}
+			state = parseStateDefault
+		}
+		i += size
+	}
+	if state != parseStateDefault {
+		return nil, errorf(p.read+i, "invalid pct-encoded")
 	}
 	exp := expNoop(p.r[:i])
 	p.dropN(i)
